b0bd0a4ca0fcb003baf8727c4eadf4e675645545
leleqiang <1427321452@qq.com>
2018-05-11 15:23:41 +0800

    变量整合

--- src/main.js
+++ src/main.js
@@ -9,36 +9,11 @@ import store from './models';
 import config from './config'
 //import { TMap } from 'TMap'
 //Vue.use(TMap)
-Vue.config.productionTip = false;Vue.config.test =9; 
+Vue.config.productionTip = false;
 
 Vue.mixin({
 	data() {
-		return {
-			root: '',
-			selectData: [],
-			curPage: 1, //1列表页 2新增编辑页 3详情页
-			saveType: 0, //保存类型 0新增 1编辑
-			eformType: 0, //内嵌套弹框保存类型 0新增 1编辑
-			totalNum: 0,
-			tLoading: false, //表格加载
-			fLoading: false, //表单加载
-			bLoading: false, //按钮加载
-			sels: [], //列表选中列
-			formVisible: false, //新增编辑界面是否显示
-			detailVisible: false, //详情界面是否显示
-			advanced: false, //是否显示高级查询
-			detailData: {},
-			box: '',
-			container: '',
-			containerTop: '',
-			scrollTop: '20px',
-			codeUrl: '/admin/system/sysDict/findAllChildDictForSelect.do', //编码字典接口
-			provinceData: [],
-			cityData: [],
-			areaData: [],
-			cxCityData: [],
-			cxAreaData: []
-		}
+		return config
 	},
 	methods: {
 		//vue-resource
