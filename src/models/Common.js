c4b809ecdfbc798d73b47e5e031308bf1afa8811
wang zaron <wangzaron@wangs-iMac.local>
2018-08-10 15:35:38 +0800

    add transformer

--- src/models/Common.js
+++ src/models/Common.js
@@ -2,15 +2,17 @@ import Model from './Model'
 export default class Common extends Model{
   constructor(application) {
     super(application);
+    this.provinces = [];
   }
   data(){
+    let self = this;
     return {
     	saveType: 0,
 			container: '',
 			box:'',
 			containerTop: '',
 			scrollTop: '20px',
-      provinces: [ ],
+      provinces: self.provinces,
     };
   }
   computed() {
