85d24e3f174d345109ce179a400e57b7dbb03e17
leleqiang <1427321452@qq.com>
2018-08-15 09:12:06 +0800

    update

--- src/services/http/ShopsService.js
+++ src/services/http/ShopsService.js
@@ -8,21 +8,9 @@ export default class ShopsService extends ApiService{
 		let totalNum = 0;
 		let currentPage = 0;
 		let pageCount = 0;
-<<<<<<< HEAD
 		let response = null;
 		if(this.$application.mock()) {
 			response =  await this.services('shopsMock').mock(page, search, limit);
-=======
-		let totalPage = 0;
-		if(this.$application.mock()) {
-			let response =  await this.services('shopsMock').mock(page, search, limit);
-			shops = response.data;
-			let pagination = response.meta.pagination;
-			totalNum = pagination.total;
-			currentPage = pagination['current_page'];
-			totalPage = pagination['total_pages'];
-			pageCount = pagination['per_page'];
->>>>>>> 65198ab942e97cf028beca8617821909382525aa
 		}else{
 			//服务器交互代码
 		}
