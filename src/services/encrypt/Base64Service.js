af03256afe69ef38fc3ed7299e8a4c7424e39979
wang zaron <wangzaron@wangs-iMac.local>
2018-08-16 10:58:33 +0800

    添加api 搜索添加组装函数

--- src/services/encrypt/Base64Service.js
+++ src/services/encrypt/Base64Service.js
@@ -1,10 +1,14 @@
 import {base64} from 'js-base64';
 import Service from '../Service';
+import _ from 'underscore';
 export default class Base64Service extends Service{
   constructor(application) {
     super(application);
   }
   encode(data) {
+    if(!_.isString(data)){
+      data = this.services('json').encode(data); 
+    }
     return base64.encode(data);
   }
   encodeURI(data) {
--- src/services/http/FullReduceService.js
+++ src/services/http/FullReduceService.js
@@ -3,4 +3,8 @@ export default class FullReduceService extends ApiService{
 	constructor(application) {
 		super(application);
 	}
+	//组装搜索字段
+	buildSearchStr(searchFields) {
+		return {'status': searchFields['status']};
+	}
 }
