a3aded3d768afb9943e2914e9ac743580a8d3c7b
ZoranWang <wangzaron@wangs-iMac.local>
2018-09-30 22:17:39 +0800

    跨域授权

--- src/views/projects/OpenPlatformAuth.vue
+++ src/views/projects/OpenPlatformAuth.vue
@@ -2,12 +2,12 @@
   <el-dialog
 		title="公众号/小程序授权"
 		:visible.sync="show"
-		width="60%"
+		width="80%"
     @open="open"
     @close="close"
 		center>
 		<span>
-			<iframe src="http://www.youzan.com/v2/shop/weixin/binding" id="open_platform_auth" name="open_platform_auth" style="width: 100%;" ></iframe>
+			<iframe scrolling="no" src="http://www.youzan.com/v2/shop/weixin/binding" id="open_platform_auth" name="open_platform_auth" style="width: 100%;" ></iframe>
 		</span>
 		<span slot="footer" class="dialog-footer">
 			<el-button @click="openPlatformAuthShow = false;">取 消</el-button>
@@ -40,3 +40,28 @@ export default {
   }
 }
 </script>
+<style>
+iframe{
+	width: 340px;
+	margin: 8px 0;
+	height: 500px;
+	position: relative;
+	overflow: hidden;
+	border: 1px solid #a0a0a0;
+}
+body{
+  min-width: 100% !important;
+}
+iframe html{
+	margin: -219px -150px 0 -150px;
+  width: 100%;
+	height: 480px;
+}
+
+iframe html .mp-body{
+	padding-bottom: 0;
+}
+iframe html .mp-foot{
+	display: none;
+}
+</style>
