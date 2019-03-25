a4bb4345bad9d09d3886c9a5ac83a5af6cc8381b
katherine <1134052988@qq.com>
2019-03-25 10:27:44 +0800

    update 广告投放

--- src/views/marketing/advertisement/AdvertisementForm.vue
+++ src/views/marketing/advertisement/AdvertisementForm.vue
@@ -1,64 +1,91 @@
 <template>
-    <el-form :model="advertisement" label-width="150px" ref="advertisementForm" :rules="rules">
-        <div class="">
-            <h4>基本信息</h4>
-            <el-form-item label="广告标题：" prop="title">
-                <el-input v-model="advertisement.title" clearable size="small" style="width: 360px;" maxlength="20"
-                          placeholder="最多可输入20个字符"></el-input>
-            </el-form-item>
-            <el-form-item label="广告图：" prop="banner_url">
-                <el-upload class="avatar-uploader"
-                           action=""
-                           :http-request="uploadAdvertisementImage"
-                           :show-file-list="false"
-                           :on-success="imageUploadHandler"
-                           :before-upload="beforeAdvertisementImageUpload">
-                    <img v-if="advertisement.banner_url" :src="advertisement.banner_url" class="avatar" alt=""
-                         style="width: 100%;height: 100%;">
-                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-                    <div slot="tip" class="el-upload__tip">建议尺寸：670 × 198</div>
-                </el-upload>
-            </el-form-item>
-            <el-form-item label="关联优惠券：" prop="card_id">
-                <!--<el-button v-if="advertisement.card_id"></el-button>-->
-                <table-list :service="tickets.table_list.service" :event="tickets.table_list.event"
-                            :current="tickets.table_list.current" :model="cardModel"
-                            :query="tickets.table_list.query">
-                    <template slot="header" slot-scope="{ search, searchHandler }">
-                        <el-input v-model="tickets.table_list.search" class="query-input" style="width: 200px;"
-                                  size="small" prefix-icon="el-icon-search" placeholder="请输入搜索内容"></el-input>
-                        <card-header v-model="search" @search="searchHandler"></card-header>
+    <div>
+        <el-form :model="advertisement" label-width="150px" ref="advertisementForm" :rules="rules">
+            <div class="">
+                <h4>基本信息</h4>
+                <el-form-item label="广告标题：" prop="title">
+                    <el-input v-model="advertisement.title" clearable size="small" style="width: 360px;" maxlength="20"
+                              placeholder="最多可输入20个字符"></el-input>
+                </el-form-item>
+                <el-form-item label="广告图：" prop="banner_url">
+                    <el-upload class="avatar-uploader"
+                               action=""
+                               :http-request="uploadAdvertisementImage"
+                               :show-file-list="false"
+                               :on-success="imageUploadHandler"
+                               :before-upload="beforeAdvertisementImageUpload">
+                        <img v-if="advertisement.banner_url" :src="advertisement.banner_url" class="avatar" alt=""
+                             style="width: 100%;height: 100%;">
+                        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                        <div slot="tip" class="el-upload__tip">建议尺寸：670 × 198</div>
+                    </el-upload>
+                </el-form-item>
+                <el-form-item label="关联优惠券：" prop="card_id">
+                    <el-button type="success" v-if="advertisement.card_id" size="small"
+                               @click="advertisementSelectFormPopupHandler(true)">更换关联优惠券
+                    </el-button>
+                    <el-button type="success" v-else size="small" @click="advertisementSelectFormPopupHandler(true)">
+                        添加关联优惠券
+                    </el-button>
+                    <template v-if="advertisement.card_id">
+                        <div style="display: block;">
+                            <img src="../../../assets/ticket-left.png" alt="" class="el-card-ticket-left">
+                            <div class="el-card-div">
+                                <el-card class="el-card">
+                                    <div style="padding: 14px;">
+                                        <span></span>
+                                        <div class="bottom clearfix">
+                                            <span class="ticket-title">{{selectedTicket.data.title}}</span>
+                                            <span class="ticket-content">{{selectedTicket.data.content}}</span>
+                                        </div>
+                                    </div>
+                                </el-card>
+                                <img src="../../../assets/ticket-quan.png" alt="" class="el-card-ticket-quan">
+                            </div>
+                        </div>
                     </template>
-                    <template slot="table" slot-scope="{data}">
-                        <card-table :coupons="data" :relateCoupon="advertisement.card_id"
-                                    @relateCouponChange="relateCouponChangeHandler"></card-table>
-                    </template>
-                </table-list>
-            </el-form-item>
-            <el-form-item label="广告投放起止时间：" prop="start_to_end">
-                <el-date-picker v-model="advertisement.start_to_end" type="daterange" range-separator="至"
-                                start-placeholder="开始日期" end-placeholder="结束日期" size="small">
-                </el-date-picker>
-            </el-form-item>
-        </div>
-        <div class="">
-            <h4>投放条件</h4>
-            <el-form-item label="性别：" prop="conditions.sex">
-                <el-radio-group v-model="advertisement.conditions.sex" style="display: flex;">
-                    <el-radio label="ALL">不限</el-radio>
-                    <el-radio label="MALE">男性</el-radio>
-                    <el-radio label="FEMALE">女性</el-radio>
-                </el-radio-group>
-            </el-form-item>
-            <el-form-item label="单笔订单实付金额：" prop="conditions.payment_amount">
-                <el-input placeholder="输入金额 不填默认为0" v-model.number="advertisement.conditions.payment_amount"
-                          style="width: 250px;" size="small">
-                    <template slot="prepend">￥</template>
-                    <template slot="append">元</template>
-                </el-input>
-            </el-form-item>
-        </div>
-    </el-form>
+                </el-form-item>
+                <el-form-item label="广告投放起止时间：" prop="start_to_end">
+                    <el-date-picker v-model="advertisement.start_to_end" type="daterange" range-separator="至"
+                                    start-placeholder="开始日期" end-placeholder="结束日期" size="small">
+                    </el-date-picker>
+                </el-form-item>
+            </div>
+            <div class="">
+                <h4>投放条件</h4>
+                <el-form-item label="性别：" prop="conditions.sex">
+                    <el-radio-group v-model="advertisement.conditions.sex" style="display: flex;">
+                        <el-radio label="ALL">不限</el-radio>
+                        <el-radio label="MALE">男性</el-radio>
+                        <el-radio label="FEMALE">女性</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="单笔订单实付金额：" prop="conditions.payment_amount">
+                    <el-input placeholder="输入金额 不填默认为0" v-model.number="advertisement.conditions.payment_amount"
+                              style="width: 250px;" size="small">
+                        <template slot="prepend">￥</template>
+                        <template slot="append">元</template>
+                    </el-input>
+                </el-form-item>
+            </div>
+        </el-form>
+
+        <el-dialog title="关联优惠券" :visible.sync="dialogTableVisible">
+            <table-list :service="tickets.table_list.service" :event="tickets.table_list.event"
+                        :current="tickets.table_list.current" :model="cardModel"
+                        :query="tickets.table_list.query">
+                <template slot="header" slot-scope="{ search, searchHandler }">
+                    <el-input v-model="tickets.table_list.search" class="query-input" style="width: 200px;"
+                              size="small" prefix-icon="el-icon-search" placeholder="请输入搜索内容"></el-input>
+                    <!--<card-header v-model="search" @search="searchHandler"></card-header>-->
+                </template>
+                <template slot="table" slot-scope="{data}">
+                    <card-table :coupons="data" :relateCoupon="advertisement.card_id"
+                                @relateCouponChange="relateCouponChangeHandler"></card-table>
+                </template>
+            </table-list>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
@@ -108,6 +135,10 @@
         },
         data() {
             return {
+                dialogTableVisible: false,
+                selectedTicket: {
+                    data: null,
+                },
                 tickets: {
                     data: [],
                     page: 1,
@@ -150,29 +181,9 @@
                 }
             }
         },
-        async mounted() {
-            // await this.getTicketList();
-        },
         methods: {
-            async getTicketList() {
-                let headers = {};
-                if (this.$requestInput('projectId')) {
-                    headers = {'ProjectId': this.$requestInput('projectId')}
-                }
-
-                let search = this.tickets.table_list.query;
-                search = this.json.encode(search);
-                search = encodeURIComponent(search);
-                search = this.base64.encodeURI(search);
-
-                let [list, totalNum, currentPage, totalPage] = await this.$service('http.couponCards')
-                    .header(headers)
-                    .list(this.tickets.page, search, this.tickets.limit);
-
-                this.tickets.data = list;
-                this.tickets.page = currentPage;
-                this.tickets.totalPage = totalPage;
-                this.tickets.totalNum = totalNum;
+            advertisementSelectFormPopupHandler(popup) {
+                this.dialogTableVisible = popup;
             },
             beforeAdvertisementImageUpload(file) {
                 const isJPG = file.type === 'image/jpeg';
@@ -196,8 +207,9 @@
                     }
                 });
             },
-            relateCouponChangeHandler(card_id) {
-                this.advertisement.card_id = card_id;
+            relateCouponChangeHandler(ticket) {
+                this.advertisement.card_id = ticket ? ticket.cardId : null;
+                this.selectedTicket.data = ticket;
             }
         }
     }
@@ -205,11 +217,7 @@
 
 <style scoped>
     .query-input {
-        margin: 6px 12px 6px 6px !important;
-        float: right;
-        position: absolute;
-        right: 0;
-        z-index: 1000;
+        margin-bottom: 10px;
     }
 
     .avatar-uploader .el-upload {
@@ -237,5 +245,37 @@
         width: 178px;
         height: 178px;
         display: block;
+    }
+
+    .ticket-title {
+        font-size: 30px;
+    }
+
+    .ticket-content {
+        display: block;
+    }
+
+    .el-card {
+        border: 0px;
+        border-radius: 0px;
+        height: 120px;
+        width: 300px;
+    }
+
+    .el-card-div {
+        display: inline-block;
+        position: relative;
+    }
+
+    .el-card-ticket-left {
+        height: 120px;
+        width: 14px
+    }
+
+    .el-card-ticket-quan {
+        width: 50px;
+        position: absolute;
+        top: 0px;
+        right: 0px;
     }
 </style>
--- src/views/marketing/advertisement/EmbedCouponTable.vue
+++ src/views/marketing/advertisement/EmbedCouponTable.vue
@@ -1,22 +1,52 @@
 <template>
-    <el-table highlight-current-row :data="coupons">
-        <el-table-column prop="code" label="优惠券编号" min-width="80"/>
-        <el-table-column prop="title" label="优惠券名称" min-width="100"/>
-        <el-table-column prop="type" label="类型" min-width="120"/>
-        <el-table-column prop="content" label="优惠" min-width="120"/>
-        <el-table-column prop="activeTime" label="有效时间" min-width="100"/>
-        <el-table-column prop="issuedNum" label="总发行数" min-width="80"/>
-        <el-table-column label="操作" width="200">
-            <template slot-scope="scope">
-                <el-button type="primary" size="mini" v-if="relateCoupon !== scope.row.cardId"
-                           @click="relateCouponHandler(scope.row)">关联
-                </el-button>
-                <el-button type="danger" size="mini" v-if="relateCoupon === scope.row.cardId"
-                           @click="unRelateCouponHandler">取消关联
-                </el-button>
-            </template>
-        </el-table-column>
-    </el-table>
+    <!--<el-table highlight-current-row :data="coupons">-->
+    <!--<el-table-column prop="code" label="优惠券编号" min-width="80"/>-->
+    <!--<el-table-column prop="title" label="优惠券名称" min-width="100"/>-->
+    <!--<el-table-column prop="type" label="类型" min-width="120"/>-->
+    <!--<el-table-column prop="content" label="优惠" min-width="120"/>-->
+    <!--<el-table-column prop="activeTime" label="有效时间" min-width="100"/>-->
+    <!--<el-table-column prop="issuedNum" label="总发行数" min-width="80"/>-->
+    <!--<el-table-column label="操作" width="200">-->
+    <!--<template slot-scope="scope">-->
+    <!--<el-button type="primary" size="mini" v-if="relateCoupon !== scope.row.cardId"-->
+    <!--@click="relateCouponHandler(scope.row)">关联-->
+    <!--</el-button>-->
+    <!--<el-button type="danger" size="mini" v-if="relateCoupon === scope.row.cardId"-->
+    <!--@click="unRelateCouponHandler">取消关联-->
+    <!--</el-button>-->
+    <!--</template>-->
+    <!--</el-table-column>-->
+    <!--</el-table>-->
+
+    <ul class="coupon-list">
+        <li v-for="coupon in coupons" :key="coupon.id" class="coupon-li">
+            <div style="display: flex;width: 100%">
+                <img src="../../../assets/ticket-left.png" alt="" class="el-card-ticket-left">
+                <div class="el-card-div">
+                    <el-card class="el-card" :body-style="{ padding: '0px' }">
+                        <div style="padding: 14px;">
+                            <span></span>
+                            <div class="bottom clearfix">
+                                <span class="ticket-title">{{coupon.title}}</span>
+                                <span class="ticket-content">{{coupon.content}}</span>
+                                <span class="ticket-validate">有效期：{{coupon.activeTime}}</span>
+                            </div>
+                        </div>
+                    </el-card>
+                    <img src="../../../assets/ticket-quan.png" alt="" class="el-card-ticket-quan">
+                    <el-button title="点击关联" size="mini" circle class="coupon-relate-button" icon="el-icon-check"
+                               v-if="relateCoupon !== coupon.cardId"
+                               @click="relateCouponHandler(coupon)">
+                    </el-button>
+                    <el-button title="点击取消关联" icon="el-icon-check" circle class="coupon-relate-button" type="success"
+                               v-else
+                               size="mini" @click="unRelateCouponHandler">
+                    </el-button>
+                </div>
+            </div>
+        </li>
+    </ul>
+
 </template>
 
 <script>
@@ -37,7 +67,7 @@
         },
         methods: {
             relateCouponHandler(ticket) {
-                this.$emit('relateCouponChange', ticket.cardId);
+                this.$emit('relateCouponChange', ticket);
             },
             unRelateCouponHandler() {
                 this.$emit('relateCouponChange', null);
@@ -47,5 +77,56 @@
 </script>
 
 <style scoped>
+    .coupon-list {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-content: space-between;
+    }
 
+    .coupon-li {
+        width: 33%;
+        padding: 7px;
+    }
+
+    .ticket-title {
+        font-size: 20px;
+    }
+
+    .ticket-content {
+        font-size: 10px;
+        display: block;
+    }
+
+    .ticket-validate {
+        font-size: 10px;
+    }
+
+    .el-card {
+        border: 0px;
+        border-radius: 0px;
+        height: 100px;
+    }
+
+    .el-card-div {
+        position: relative;
+        width: 100%;
+    }
+
+    .el-card-ticket-left {
+        height: 100px;
+    }
+
+    .el-card-ticket-quan {
+        width: 42px;
+        position: absolute;
+        top: 0px;
+        right: 0px;
+    }
+
+    .coupon-relate-button {
+        position: absolute;
+        right: 5px;
+        bottom: 5px;
+    }
 </style>
