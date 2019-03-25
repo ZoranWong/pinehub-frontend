aae4a312481ff73ef106a029d49ecf7462092c33
katherine <1134052988@qq.com>
2019-03-25 10:28:55 +0800

    update

--- src/views/marketing/advertisement/EmbedCouponTable.vue
+++ src/views/marketing/advertisement/EmbedCouponTable.vue
@@ -1,23 +1,4 @@
 <template>
-    <!--<el-table highlight-current-row :data="coupons">-->
-    <!--<el-table-column prop="code" label="优惠券编号" min-width="80"/>-->
-    <!--<el-table-column prop="title" label="优惠券名称" min-width="100"/>-->
-    <!--<el-table-column prop="type" label="类型" min-width="120"/>-->
-    <!--<el-table-column prop="content" label="优惠" min-width="120"/>-->
-    <!--<el-table-column prop="activeTime" label="有效时间" min-width="100"/>-->
-    <!--<el-table-column prop="issuedNum" label="总发行数" min-width="80"/>-->
-    <!--<el-table-column label="操作" width="200">-->
-    <!--<template slot-scope="scope">-->
-    <!--<el-button type="primary" size="mini" v-if="relateCoupon !== scope.row.cardId"-->
-    <!--@click="relateCouponHandler(scope.row)">关联-->
-    <!--</el-button>-->
-    <!--<el-button type="danger" size="mini" v-if="relateCoupon === scope.row.cardId"-->
-    <!--@click="unRelateCouponHandler">取消关联-->
-    <!--</el-button>-->
-    <!--</template>-->
-    <!--</el-table-column>-->
-    <!--</el-table>-->
-
     <ul class="coupon-list">
         <li v-for="coupon in coupons" :key="coupon.id" class="coupon-li">
             <div style="display: flex;width: 100%">
