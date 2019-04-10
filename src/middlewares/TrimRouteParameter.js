ffb602a753d6dd806b9455d13afc3177a14b6474
katherine <1134052988@qq.com>
2019-04-10 11:15:11 +0800

    fix bug

--- src/middlewares/TrimRouteParameter.js
+++ src/middlewares/TrimRouteParameter.js
@@ -1,24 +1,37 @@
 import Middleware from './Middleware';
 
 export default class TrimRouteParameter extends Middleware {
+    constructor($application) {
+        super($application);
+    }
+
+
     async handle(request) {
+        if (this.checkExceptRoute(request['route'])) {
+            return;
+        }
+
         let queryParameter = request.params;
         for (let key in queryParameter) {
             if (!queryParameter[key]) {
                 delete queryParameter[key];
             }
             if (key === 'searchJson') {
+
                 let parsedSearchJson = this.$application['base64'].decodeURI(queryParameter[key]);
                 parsedSearchJson = this.$application['json'].decode(parsedSearchJson);
                 parsedSearchJson = this.$application['uri'].decodeURIComponent(parsedSearchJson);
                 parsedSearchJson = JSON.parse(parsedSearchJson);
+
                 for (let search in parsedSearchJson) {
                     if (parsedSearchJson[search] === null || parsedSearchJson[search] === 'undefined') {
                         delete parsedSearchJson[search];
                     }
                 }
+
                 parsedSearchJson = this.$application['json'].encode(parsedSearchJson);
-                parsedSearchJson = this.$application['base64'].encode(parsedSearchJson);
+                parsedSearchJson = encodeURIComponent(parsedSearchJson);
+                parsedSearchJson = this.$application['base64'].encodeURI(parsedSearchJson);
                 queryParameter[key] = parsedSearchJson;
             }
         }
