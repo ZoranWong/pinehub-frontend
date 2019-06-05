b0d0055d81147d9fc96b30298e5087db6c0862d7
katherine <1134052988@qq.com>
2019-06-05 14:51:04 +0800

    init 数据统计

--- src/models/transformers/Category.js
+++ src/models/transformers/Category.js
@@ -3,10 +3,12 @@ export default class Category {
         this.idx = category['index'];
         this.id = category['id'];
         this.name = category['name'];
-        let children = category['children']['data'];
         this.children = [];
-        for (let i in children) {
-            this.children.push(new Category(children[i]));
+        if (category.hasOwnProperty('children')) {
+            let children = category['children']['data'];
+            for (let i in children) {
+                this.children.push(new Category(children[i]));
+            }
         }
         this.key = category['key'];
         this.merchandiseNum = category['merchandise_num'];
